f9bea4cc474c99971cf4de58e6a86c8ebfe6c111
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2020-04-11 20:04:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,18 @@ function wrapAdjective(param = "*"){
     return `You are ${param}${adj}${param}!`
   }
 }
+
+let Calculator = {
+    add: function (a, b) {
+        return a + b
+    }, 
+    subtract: function(a, b) {
+        return a - b
+    },
+    multiply: function(a, b) {
+        return a * b
+    },
+    divide: function(a, b) {
+        return a / b
+    }
+}
