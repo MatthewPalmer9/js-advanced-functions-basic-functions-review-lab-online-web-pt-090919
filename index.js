1f6a1443aefd16b4e53e278be82e19bfea6864e0
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2020-04-11 19:56:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ function mondayWork(activity = "go to the office"){
   return `This Monday, I will ${activity}.`
 }
 
-function wrapAdjective(){
-  
+function wrapAdjective("*"){
+  (function(param = "*"){
+    return `You are ${param}`
+  })
 }
